110fa9ebcca7a23a218fb49b82b0f0102f75a532
ellie <tachidita@gmail.com>
2019-01-28 16:45:48 -0800

    Add user date to articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 50}
     validates :description, presence: true, length: { minimum: 10, maximum: 300}
+    validates :user_id, presence: true
 end
 #rails magically create things in between (CRUD). go to rails console to modify the model     
